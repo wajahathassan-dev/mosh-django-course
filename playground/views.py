291c96941366624456576d076e7b7ebe9ef6b68c
Wajahat Hassan <wajahat.hassan@itb.local>
2023-04-12 16:58:15 +0500

    Completed ORM part

--- playground/views.py
+++ playground/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.db.models import Q, F
-from store.models import Product, OrderItem
+from store.models import Collection, Product
 
 def say_hello(request):
-    query_set = OrderItem.objects.values_list('product_id', 'product__title').order_by('product__title')
-    return render(request, 'hello.html', {'name': 'Wajahat', 'products': list(query_set)}) 
+    # collection = Collection.objects.create(title='Lemon', featured_product_id=1)
+    # collection = Collection.objects.filter(title='Lemon').update(featured_product_id=2)
+    # collection = Collection.objects.filter(featured_product_id__isnull = True).update(featured_product_id=1)
+    # Collection.objects.filter(featured_product_id=2).delete()
+
+    query_set = Collection.objects.raw('SELECT * FROM store_product')
+    return render(request, 'hello.html', {'name': 'Wajahat', 'products': query_set })
  
